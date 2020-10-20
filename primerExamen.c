52c55cfa386dbcce2b19633e5bb4c2e94c98b7b7
pedrojsanchezc <68034218+pedrojsanchezc@users.noreply.github.com>
2020-10-20 19:35:27 -0300

    Add files via upload

--- primerExamen.c
+++ primerExamen.c
@@ -33,38 +33,25 @@ int main(void)
 
 	if(cliente_init(arrayClientes,QTY_CLIENTES)==0 && publicacion_init(arrayPublicacion,QTY_PUBLICACIONES)==0)
 	{
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-94728818-1","ANDRES","TORRES");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-78567890-2","FELIPE","CAICEDO");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"30-33567897-7","VIVIANA","HERRERA");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-94888818-1","GUSTAVO","ROJAS");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"30-21345654-6","JULIANA","PORTELA");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-94728818-1","CARLOS","FLORES");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-78567890-2","TOMAS","GIL");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"30-23908765-7","CAMILA","VASQUEZ");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-96786518-1","HERNAN","PRIETO");
-		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"30-21376454-6","DONOFRIO","VARGAS");
+		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-94728818-1","MARCOS","PEREZ");
+		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"20-78567890-2","JULIETA","MARLON");
+		cliente_altaForzada(arrayClientes,QTY_CLIENTES,"30-33567897-7","MANUEL","GUZMAN");
 
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,4,"venta de lote",5);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,2,"venta de casa",2);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,5,"venta de bote",3);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,3,"venta de ropa",4);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,4,"venta de lote",5);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,4,"venta de auto",6);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,5,"venta de ropa",6);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,7,"venta de moto",3);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,4,"venta de ropa",6);
-		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,2,"venta de zapatillas",2);
+		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,4,"venta de mochila",5);
+		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,2,"venta de zapatilla",2);
+		publicacion_altaForzada(arrayPublicacion,QTY_PUBLICACIONES,5,"venta de silla",3);
+
 
 		do
 		{
 			if(utn_getNumeroString("\n\n  1-Alta Cliente\n  2-Modificar Cliente\n  3-Baja Cliente "
 				                   "\n  4-Alta Publicacion\n  5-Pausar publicacion\n  6-Reanudar Publicacion\n  "
-				             "7-Imprimir Clientes\n  8-Informar\n  9-Salir  \n  Ingrese opción:   ",
-							 "ERROR Reingrese opcion valida",&opcion,CANTIDAD_REINTENTOS,1,9)==0)
+				                   "7-Imprimir Clientes\n  8-Informar\n  9-Salir  \n  Ingrese opción:   ",
+							       "ERROR Reingrese opcion valida",&opcion,CANTIDAD_REINTENTOS,1,9)==0)
 			{
 				switch(opcion)
 				{
-					case 1://Alta Cliente
+					case 1:// Inicia Alta Cliente
 						   if(cliente_createNew(arrayClientes,QTY_CLIENTES)==0)
 						   {
 							   printf("\n  ALTA EXITOSA\n");
@@ -74,7 +61,7 @@ int main(void)
 							   printf("\n  NO SE REALIZO ALTA \n");
 						   }
 						break;
-					case 2://Modificar Cliente
+					case 2://Para Modificar Cliente
 						if(cliente_modifify(arrayClientes,QTY_CLIENTES)==0)
 						{
 							printf("\n  MODIFICACION EXITOSA\n");
@@ -84,7 +71,7 @@ int main(void)
 							printf("\n  MODIFICACION INCORECTA\n");
 						}
 					break;
-					case 3://Baja Cliente
+					case 3://Para Baja Cliente
 					    if(info_removeClientById(arrayClientes,QTY_CLIENTES,arrayPublicacion,QTY_PUBLICACIONES)==0)
 					    {
 							 printf("\n  BAJA EXITOSA\n");
@@ -94,7 +81,7 @@ int main(void)
 							printf("\n  BAJA INCORECTA\n");
 						}
 					break;
-					case 4://Alta Publicacion
+					case 4://Para Alta Publicacion
 						if(publicacion_createNewProfile(arrayPublicacion,QTY_PUBLICACIONES,arrayClientes,QTY_CLIENTES)==0)
 						{
 							   printf("\n  ALTA EXITOSA\n");
@@ -104,7 +91,7 @@ int main(void)
 				        	   printf("\n  NO SE REALIZO ALTA \n");
 					    }
 					break;
-					case 5://Pausar publicacion
+					case 5://Para Pausar publicacion
 						if(publicacion_pause(arrayPublicacion,QTY_PUBLICACIONES,arrayClientes,QTY_CLIENTES)==0)
 						{
 							printf("\n  PUBLICACION PAUSADA\n");
@@ -114,7 +101,7 @@ int main(void)
 							printf("\n  PUBLICACION NO PAUSADA, ERROR\n");
 						}
 					break;
-					case 6://Reanudar Publicacion
+					case 6://Para Reanudar Publicacion
 						if(publicacion_reanude(arrayPublicacion,QTY_PUBLICACIONES,arrayClientes,QTY_CLIENTES)==0)
 						{
 							 printf("\n  PUBLICACION ACTIVA\n");
@@ -124,7 +111,7 @@ int main(void)
 							printf("\n  PUBLICACION NO ACTIVA, ERROR \n");
 						}
 					break;
-					case 7://Imprimir clientes con todos sus datos
+					case 7://Para Imprimir clientes con todos sus datos
 						if(info_printActive(arrayClientes,QTY_CLIENTES,arrayPublicacion,QTY_PUBLICACIONES)==0)
 						{
 							printf("\n  FIN DE IMFORME\n");
@@ -134,7 +121,7 @@ int main(void)
 							printf("\n NO HAY CLIENTES CARGADOS\n");
 						}
 					break;
-					case 8://Informar
+					case 8://Para Informar
 						if(info_report(arrayClientes,QTY_CLIENTES,arrayPublicacion,QTY_PUBLICACIONES)==0)
 						{
 							printf("\n  FIN DEL INFORME\n");
